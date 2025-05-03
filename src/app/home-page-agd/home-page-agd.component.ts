d6b561656110ea9aae6d0730c17143117e9961b5
Hazeena <hazeenahussain8@gmail.com>
2025-05-04 03:40:39 +0530

    changes

--- src/app/home-page-agd/home-page-agd.component.ts
+++ src/app/home-page-agd/home-page-agd.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page-agd.component.scss']
 })
 export class HomePageAgdComponent implements OnInit {
+  itemList:any[]=[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.itemList=[
+      {id:'1',name:'Earring',qty:'45', img: '../../assets/icons/ear-ring.svg'},
+      {id:'2',name:'Bracelet',qty:'30', img: '../../assets/icons/ring.svg'},
+      {id:'3',name:'Necklace',qty:'30', img: '../../assets/icons/neck-chain.svg'},
+      {id:'4',name:'Rings',qty:'40', img: '../../assets/icons/ring1.svg'},
+      {id:'5',name:'Pendants',qty:'40', img: '../../assets/icons/ring2.svg'},
+      {id:'6',name:'Nose Pins',qty:'45', img: '../../assets/icons/ring3.svg'}
+    ]
   }
 
 }
